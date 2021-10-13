2936ff5d9d3e249237762d3d5e85e52a925124a3
irbopia <travislockett1@gmail.com>
2021-10-13 06:15:54 -0400

    Set up redirect for root route if user is logged in

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -9,62 +9,65 @@ import {
 } from "@chakra-ui/react";
 import { FcGoogle } from "react-icons/fc";
 import { SunIcon } from "@chakra-ui/icons";
+import { Route, Redirect } from "react-router-dom";
+import { useAuth } from '../../lib/AuthContext';
 
 const HomePage = () => {
 	const { toggleColorMode } = useColorMode();
 	const formBackground = useColorModeValue("gray.100");
-
-	// delete this when auth is completed and redirects to the appropiate route
-	// I just created this so that we can easily get to the habit dashboard until auth is done
 	const history = useHistory();
-	const loginRedirect = () => {
-		history.push("/login");
-	};
+	const { currentUser } = useAuth();
 
 	return (
-		<Flex
-			background={formBackground}
-			height="100vh"
-			alignItems="center"
-			justifyContent="center"
-		>
-			<main>
-				<Heading fontSize="4em" color="blue.400">
-					Habit Tribes
+		<Route
+			render={() => {
+				return currentUser ? <Redirect to='/habits' /> :
+					<Flex
+						background={formBackground}
+						height="100vh"
+						alignItems="center"
+						justifyContent="center"
+					>
+						<main>
+							<Heading fontSize="4em" color="blue.400">
+								Habit Tribes
 				</Heading>
-				<Button
-					rightIcon={<FcGoogle />}
-					backgroundColor="gray.600"
-					color="white"
-					fontWeight="medium"
-					mt={2}
-					_hover={{ bg: "gray.400" }}
-					_active={{
-						bg: "gray.800",
-						transform: "scale(0.95)",
-					}}
-					onClick={loginRedirect}
-				>
-					Login
+							<Button
+								rightIcon={<FcGoogle />}
+								backgroundColor="gray.600"
+								color="white"
+								fontWeight="medium"
+								mt={2}
+								_hover={{ bg: "gray.400" }}
+								_active={{
+									bg: "gray.800",
+									transform: "scale(0.95)",
+								}}
+								onClick={() => { history.push('/login') }}
+							>
+								Login
 				</Button>
-				<div></div>
-				<Button
-					rightIcon={<SunIcon />}
-					backgroundColor="gray.600"
-					color="white"
-					fontWeight="medium"
-					mt={2}
-					_hover={{ bg: "gray.400" }}
-					_active={{
-						bg: "gray.800",
-						transform: "scale(0.95)",
-					}}
-					onClick={toggleColorMode}
-				>
-					Color Mode
+							<div></div>
+							<Button
+								rightIcon={<SunIcon />}
+								backgroundColor="gray.600"
+								color="white"
+								fontWeight="medium"
+								mt={2}
+								_hover={{ bg: "gray.400" }}
+								_active={{
+									bg: "gray.800",
+									transform: "scale(0.95)",
+								}}
+								onClick={toggleColorMode}
+							>
+								Color Mode
 				</Button>
-			</main>
-		</Flex>
+						</main>
+					</Flex>
+			  }
+			}
+		/>
 	);
 };
 
