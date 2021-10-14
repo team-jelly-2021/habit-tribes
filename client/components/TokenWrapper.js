4e5086e13d67d63a65eddd3792cb8e0cb4edf447
irbopia <travislockett1@gmail.com>
2021-10-14 15:44:37 -0400

    Back end authentication completed

--- client/components/TokenWrapper.js
+++ client/components/TokenWrapper.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { useToken } from '../../lib/useToken'
 
 function TokenWrapper({ children }) {
-  const token = useToken()
+  const token = useToken();
   return (
     <>
      {children} 
