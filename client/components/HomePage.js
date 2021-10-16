4e86e84d9601ba6a5e43de6ae02a3aaec0e58215
irbopia <travislockett1@gmail.com>
2021-10-16 16:19:49 -0400

    Home page change

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import {
+	Text,
 	Flex,
 	Button,
 	Heading,
 	useColorMode,
 	useColorModeValue,
 } from "@chakra-ui/react";
-import { FcGoogle } from "react-icons/fc";
+import { BiLogIn } from "react-icons/bi";
+import { MdAccountCircle } from 'react-icons/md';
 import { SunIcon } from "@chakra-ui/icons";
 import { Route, Redirect } from "react-router-dom";
 import { useAuth } from '../lib/AuthContext';
@@ -21,50 +23,81 @@ const HomePage = () => {
 	return (
 		<Route
 			render={() => {
-				return currentUser ? <Redirect to='/habits' /> :
-					<Flex
-						background={formBackground}
-						height="100vh"
-						alignItems="center"
-						justifyContent="center"
-					>
-						<main>
-							<Heading fontSize="4em" color="blue.400">
-								Habit Tribes
-				</Heading>
-							<Button
-								rightIcon={<FcGoogle />}
-								backgroundColor="gray.600"
-								color="white"
-								fontWeight="medium"
+				return currentUser ? (
+          <Redirect to="/habits" />
+        ) : (
+          <Flex
+            background={formBackground}
+            height="100vh"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <main>
+              <Heading fontSize="4em">
+                <Text
+                  bgGradient="linear(to-r, blue.600, green.200)"
+                  bgClip="text"
+                  fontSize="6xl"
+                  fontWeight="extrabold"
+                >
+                  Habit Tribes
+                </Text>
+              </Heading>
+              <Button
+                rightIcon={<BiLogIn />}
+                backgroundColor="blue.600"
+                color="white"
+                fontWeight="medium"
+                mt={2}
+                _hover={{bg: 'blue.400'}}
+                _active={{
+                  bg: 'blue.600',
+                  transform: 'scale(0.95)',
+                }}
+                onClick={() => {
+                  history.push('/login');
+                }}
+              >
+                Login
+              </Button>
+              <div></div>
+              <Button
+                rightIcon={<MdAccountCircle />}
+                backgroundColor="green.400"
+                color="white"
+                fontWeight="medium"
+                mt={2}
+                _hover={{bg: 'green.300'}}
+                _active={{
+                  bg: 'green.800',
+                  transform: 'scale(0.95)',
+                }}
+                onClick={() => {
+                  history.push('/register');
+                }}
+              >
+                Create new account
+              </Button>
+              <div></div>
+								<Button
 								mt={2}
-								_hover={{ bg: "gray.400" }}
-								_active={{
-									bg: "gray.800",
-									transform: "scale(0.95)",
-								}}
-								onClick={() => { history.push('/login') }}
-							>
-								Login
-				</Button>
-							<div></div>
-							<Button
-								rightIcon={<SunIcon />}
-								backgroundColor="gray.600"
-								color="white"
-								fontWeight="medium"
-								mt={2}
-								_hover={{ bg: "gray.400" }}
-								_active={{
-									bg: "gray.800",
-									transform: "scale(0.95)",
-								}}
-								onClick={toggleColorMode}
-							>
-								Color Mode
-				</Button>
-						</main>
-					</Flex>
+                rightIcon={<SunIcon />}
+                backgroundColor="yellow.400"
+                color="white"
+                fontWeight="medium"
+                mt={2}
+                _hover={{bg: 'gray.400'}}
+                _active={{
+                  bg: 'gray.800',
+                  transform: 'scale(0.95)',
+                }}
+                onClick={toggleColorMode}
+              >
+                Color Mode
+              </Button>
+            </main>
+          </Flex>
+        );
 			  }
 			}
 		/>
